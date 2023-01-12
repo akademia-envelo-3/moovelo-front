60e71bfeeebda16131f99f4305598bed004b1126
Maciej Walecki <maciej.walecki@ppuc.pl>
2023-01-12 14:59:30 +0100

    feat: add event type form

--- src/app/features/event/components/event-type-form/event-type-form.component.ts
+++ src/app/features/event/components/event-type-form/event-type-form.component.ts
@@ -1,17 +1,19 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { EventFormProvider } from '../../event-form-provider';
 
 @Component({
   selector: 'app-event-type-form',
   templateUrl: './event-type-form.component.html',
-  styleUrls: ['./event-type-form.component.css'],
+  styleUrls: ['./event-type-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EventTypeFormComponent {
   private eventFormProvider = inject(EventFormProvider);
 
-  ngOnInit() {
-    console.log('asd');
-    console.log(this.eventFormProvider.getForm());
+  constructor() {
+    this.eventTypeForm = this.eventFormProvider.getForm().get('eventTypeForm') as FormGroup;
   }
+
+  eventTypeForm: FormGroup;
 }
