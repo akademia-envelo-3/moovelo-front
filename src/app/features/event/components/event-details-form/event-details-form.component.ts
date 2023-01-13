75aca8687f7986f65912b4622fde1e70aad1b7f4
Maciej Walecki <maciej.walecki@ppuc.pl>
2023-01-13 11:25:11 +0100

    style: changed heading construction

--- src/app/features/event/components/event-details-form/event-details-form.component.ts
+++ src/app/features/event/components/event-details-form/event-details-form.component.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { EventFormProvider } from '../../event-form-provider';
 
 @Component({
   selector: 'app-event-details-form',
   templateUrl: './event-details-form.component.html',
   styleUrls: ['./event-details-form.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EventDetailsFormComponent {
-
+  private eventFormProvider = inject(EventFormProvider);
 }
