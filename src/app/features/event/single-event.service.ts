7734c8f6516dfaf429dc222eb4378169197a2725
Anna Kierlin <anna.kierlin@ppuc.pl>
2023-01-13 09:07:14 +0100

    feat: building ngrx in progress

--- src/app/features/event/single-event.service.ts
+++ src/app/features/event/single-event.service.ts
@@ -2,14 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { combineLatest } from 'rxjs';
+import { AppState } from 'src/app/app.module';
 import { SingleEventStateInterface } from './single-event.interface';
 @Injectable({
   providedIn: 'root',
 })
 export class SingleEventService {
-  private store = inject(Store);
+  private store = inject<Store<AppState>>(Store);
   private http = inject(HttpClient);
-  constructor() {}
+  // constructor() {}
 
   getSingleEvent(eventId: string) {
     combineLatest([this.http.get<SingleEventStateInterface>(`http://localhost:3000/events/${eventId}`)]);
--- src/app/features/event/store/single-event.reducer.ts
+++ src/app/features/event/store/single-event.reducer.ts
@@ -1,11 +1,15 @@
 import { createReducer, on } from '@ngrx/store';
+import { SingleEventStateInterface } from '../single-event.interface';
 import { SingleEventActions } from './single-event.actions';
 import { initialSingleEventState } from './single-event.state';
 
 export const singleEventReducer = createReducer(
   initialSingleEventState,
-  on(SingleEventActions.fetch_single_event, (state, { eventId }) => ({
-    ...state,
-    eventId: eventId,
-  }))
+  on(
+    SingleEventActions.fetch_single_event,
+    (state, { eventId }): SingleEventStateInterface => ({
+      ...state,
+      eventId: eventId,
+    })
+  )
 );
