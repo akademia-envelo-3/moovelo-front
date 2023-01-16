dfd113ddd6f53ac9f49162b763e20050fe4c2958
Maciej Walecki <maciej.walecki@ppuc.pl>
2023-01-16 12:43:14 +0100

    feat: add hour validators

--- src/app/features/event/components/create-event/create-event.component.ts
+++ src/app/features/event/components/create-event/create-event.component.ts
@@ -1,5 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import patterns from '@shared/regex-patterns';
 import { EventFormProvider } from '../../event-form-provider';
 
 export interface EventForm {
@@ -31,6 +39,23 @@ export interface EventDetailsForm {
   hashtags: FormControl<string[]>;
 }
 
+export const isHourInThePastValidator: ValidatorFn = (control: AbstractControl) => {
+  const today = new Date();
+  const startDateRaw = control.get('startDate')?.value as string | undefined;
+  const hour = control.get('hour')?.value as string | undefined;
+  if (startDateRaw && hour) {
+    const startDate = new Date(startDateRaw);
+    if (startDate.toDateString() === today.toDateString()) {
+      const hourSplitted = hour.split(':');
+      return today.getHours() < +hourSplitted[0] ||
+        (today.getHours() === +hourSplitted[0] && today.getMinutes() < +hourSplitted[1])
+        ? null
+        : { isHourInThePast: true };
+    }
+  }
+  return null;
+};
+
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
@@ -53,27 +78,38 @@ export class CreateEventComponent extends EventFormProvider {
         isPrivate: this.builder.control(false),
         isGroup: this.builder.control(false),
       }),
-      eventDetailsForm: this.builder.group({
-        group: this.builder.control<number | null>(null),
-        limitedPlaces: this.builder.control(0, {
-          validators: [Validators.min(1)],
-        }),
-        isLimitedPlaces: this.builder.control(false),
-        isConfirmationRequired: this.builder.control(false),
-        name: this.builder.control('', {
-          validators: [Validators.required, Validators.minLength(4), Validators.maxLength(100)],
-        }),
-        category: this.builder.control<string[]>([]),
-        startDate: this.builder.control(''),
-        hour: this.builder.control(''),
-        postCode: this.builder.control(''),
-        city: this.builder.control(''),
-        street: this.builder.control(''),
-        streetNumber: this.builder.control(''),
-        apartmentNumber: this.builder.control(''),
-        description: this.builder.control(''),
-        hashtags: this.builder.control<string[]>([]),
-      }),
+      eventDetailsForm: this.builder.group(
+        {
+          group: this.builder.control<number | null>(null),
+          limitedPlaces: this.builder.control(0, {
+            validators: [Validators.required, Validators.min(1)],
+          }),
+          isLimitedPlaces: this.builder.control(false),
+          isConfirmationRequired: this.builder.control(false),
+          name: this.builder.control('', {
+            validators: [Validators.required, Validators.minLength(4), Validators.maxLength(100)],
+          }),
+          category: this.builder.control<string[]>([]),
+          startDate: this.builder.control('', {
+            validators: [Validators.required],
+          }),
+          hour: this.builder.control('', {
+            validators: [Validators.required],
+          }),
+          postCode: this.builder.control('', {
+            validators: [Validators.required, Validators.pattern(patterns.postCode)],
+          }),
+          city: this.builder.control('', {
+            validators: [Validators.required],
+          }),
+          street: this.builder.control(''),
+          streetNumber: this.builder.control(''),
+          apartmentNumber: this.builder.control(''),
+          description: this.builder.control(''),
+          hashtags: this.builder.control<string[]>([]),
+        },
+        { validators: isHourInThePastValidator }
+      ),
     });
   }
 }
--- src/app/features/event/components/event-details-form/event-details-form.component.ts
+++ src/app/features/event/components/event-details-form/event-details-form.component.ts
@@ -1,7 +1,16 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { AbstractControl, FormGroup, FormGroupDirective, NgForm } from '@angular/forms';
 import { EventFormProvider } from '../../event-form-provider';
 import { EventTypeForm, EventDetailsForm } from '../create-event/create-event.component';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class HourErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: AbstractControl<any, any> | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    if (form?.hasError('isHourInThePast')) return true;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-event-details-form',
@@ -15,13 +24,26 @@ export class EventDetailsFormComponent {
   constructor() {
     this.eventTypeForm = this.eventFormProvider.getForm().get('eventTypeForm') as FormGroup<EventTypeForm>;
     this.eventDetailsForm = this.eventFormProvider.getForm().get('eventDetailsForm') as FormGroup<EventDetailsForm>;
-    this.eventDetailsForm.valueChanges.subscribe(r => {
-      console.log(this.nameCtrl.errors);
-    });
   }
 
   eventTypeForm: FormGroup<EventTypeForm>;
   eventDetailsForm: FormGroup<EventDetailsForm>;
+  today = new Date();
+  hourMatcher = new HourErrorStateMatcher();
+
+  ngOnInit() {
+    this.isLimitedPlacesCtrl.valueChanges.subscribe(value => {
+      if (value) {
+        this.limitedPlacesCtrl.enable();
+      } else {
+        this.limitedPlacesCtrl.disable();
+      }
+    });
+  }
+
+  handleClick() {
+    console.log(this.postCodeCtrl.errors);
+  }
 
   get title() {
     const value = this.eventTypeForm.value;
@@ -42,5 +64,17 @@ export class EventDetailsFormComponent {
 
   get limitedPlacesCtrl() {
     return this.eventDetailsForm.controls.limitedPlaces;
+  }
+
+  get startDateCtrl() {
+    return this.eventDetailsForm.controls.startDate;
+  }
+
+  get hourCtrl() {
+    return this.eventDetailsForm.controls.hour;
+  }
+
+  get postCodeCtrl() {
+    return this.eventDetailsForm.controls.postCode;
   }
 }
