4236d3105bc33dd2df4aa657c929187c3468f7bd
Jakub Borkowski <jakub.borkowski@ppuc.pl>
2023-01-16 15:39:15 +0100

    fix: changed styling and added message about redirection

--- src/app/shared/error404/error404.component.ts
+++ src/app/shared/error404/error404.component.ts
@@ -1,22 +1,21 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule],
   selector: 'app-error404',
   templateUrl: './error404.component.html',
   styleUrls: ['./error404.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Error404Component implements OnInit {
-  constructor(private router: Router) {}
+  private router = inject(Router);
 
   goBack(): void {
     setTimeout(() => {
       this.router.navigateByUrl('/home');
-    }, 6000);
+    }, 5000);
   }
 
   ngOnInit(): void {
