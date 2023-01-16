4e2121a07f1104f451cef83acfb0ab8487048a07
Jakub Borkowski <jakub.borkowski@ppuc.pl>
2023-01-16 14:50:48 +0100

    fix: changed styles and added emailto

--- src/app/core/theme.component.ts
+++ src/app/core/theme.component.ts
@@ -5,7 +5,7 @@ import { FooterComponent } from '@shared/footer/footer.component';
   selector: 'app-theme',
   standalone: true,
   imports: [FooterComponent],
-  template: ` <h1>Storybook-like route</h1>
+  template: ` <h1>Footer component</h1>
     <app-footer></app-footer>`,
 })
 export default class ThemeComponent {}
