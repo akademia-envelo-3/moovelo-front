3b6d82ab706f0bcd5f1473f10476177328d256c5
Grzegorz Bandyra <czesiox8@gmail.com>
2023-01-17 14:26:20 +0100

    fix: api url and palette import

--- src/app/features/group/group-list/group-list.service.ts
+++ src/app/features/group/group-list/group-list.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { API_URL } from '@core/env.token';
 import { BehaviorSubject, catchError, of, retry } from 'rxjs';
 import { GroupListItem } from '../group.interface';
 import { GroupListError } from '../group.interface';
@@ -7,15 +8,15 @@ import { GroupListError } from '../group.interface';
 @Injectable()
 export class GroupListService {
   private http = inject(HttpClient);
-  private groupUrl = 'http://localhost:3000/groups';
   private error$$ = new BehaviorSubject<GroupListError>({ isError: false, errorStatus: 404 });
+  private apiUrl = inject(API_URL);
 
   get error$() {
     return this.error$$.asObservable();
   }
 
   getAllGroups() {
-    return this.http.get<GroupListItem[]>(this.groupUrl).pipe(
+    return this.http.get<GroupListItem[]>(this.apiUrl).pipe(
       retry(3),
       catchError(err => {
         this.error$$.next({ isError: true, errorStatus: err.status });
