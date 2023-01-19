e1ba352ba7b5ac7eaf4ce6eabb8353ac8ab288a8
Grzegorz Bandyra <czesiox8@gmail.com>
2023-01-19 16:49:30 +0100

    feat: add single group interface

--- src/app/features/group/group.interface.ts
+++ src/app/features/group/group.interface.ts
@@ -3,3 +3,50 @@ export interface GroupListItem {
   name: string;
   description: string;
 }
+
+export interface SingleGroup {
+  id: number;
+  name: string;
+  description: string;
+  groupOwner: SingleGroupOwner;
+  groupMembers: SingleGroupMembers[];
+  events: SingleGroupEvents[];
+}
+
+export interface SingleGroupOwner {
+  basicUserId: number;
+}
+
+export interface SingleGroupMembers {
+  id: number;
+  firstName: string;
+  lastName: string;
+}
+
+export interface SingleGroupEvents {
+  id: number;
+  eventInfo: EventInfo;
+}
+
+export interface EventInfo {
+  name: string;
+  category: Category;
+  startDate: string;
+  hashtags: Hashtag[];
+  isConfirmationRequired: boolean;
+  isPrivate: boolean;
+  group: boolean;
+  cycleLength: number;
+  city: string;
+  acceptedStatusUsers: number;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export interface Hashtag {
+  id: number;
+  value: string;
+}
---
+++ src/app/features/group/single-group/single-group.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { API_URL } from '@core/env.token';
+import { SingleGroup } from '../group.interface';
+
+@Injectable()
+export class SingleGroupService {
+  private http = inject(HttpClient);
+  private apiUrl = inject(API_URL);
+
+  getGroupInfo(groupId: number) {
+    return this.http.get<SingleGroup>(this.apiUrl + `/groups/${groupId}`);
+  }
+}
