cb4bcf2ab1b8b4d2df9a2d758330a847cfc40854
Łukasz Pietrzykowski <89263051+LukaszPietrzykowski16@users.noreply.github.com>
2023-01-19 09:56:36 +0100

    Feature #69 user widzi navbar (#82)
    * feat: loader cow component
    
    * fix: displaying cow loader in the /theme route instead of front page route
    
    * fix: styles: paddign change and remove comment
    
    * fix: styles: padding change to px, add: alt atribute on image
    
    * fix: fix the empty lines and spaces, fixing my prettier config
    
    * feat: styles and navbar component
    
    * fix: remove cow-loader.component.ts
    
    * fix: change of styles, classes in navbar.component, add svg file instead of png
    
    * feat: add icons to navbar
    
    * feat: creating navbar-scroll.service
    
    * feat: showing menu and chaning icons on click
    
    * feat: displaying menu on click
    
    * feat: navbar menu depends on object
    
    * feat: changing styles in scss
    
    * fix: changing navbar component to standalone
    
    * fix: removing unused import
    
    * fix: fixing BEM classes
    
    * fix: add router to the navbar
    
    * fix: changing name of function, removing comments and console.log
    
    * fix: changing files architecture, css class rename
    
    * fix: changing name of scss files
    
    * fix: changing colors in navbar

---
+++ src/app/shared/user-navbar/mock/users.config.ts
@@ -0,0 +1,11 @@
+const users = {
+  admin: {
+    titles: ['Wydarzenia', 'Propozycje kategorii', 'Kategorie', 'Grupy', 'Hashtsagi', 'Wyloguj'],
+    routes: ['events', 'category-proposition', 'category', 'groups', 'hashtags', 'logout'],
+  },
+  user: {
+    titles: ['Wydarzenia', 'Moje wydarzenia', 'Grupy', 'Moje grupy', 'Wyloguj'],
+    routes: ['events', 'my-events', 'groups', 'my-groups', 'logout'],
+  },
+};
+export default users;
