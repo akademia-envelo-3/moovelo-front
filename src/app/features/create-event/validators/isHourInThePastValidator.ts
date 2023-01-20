45460d2948fec9e321b75b012fe9b8a2ad2f66f5
Maciej Walecki <maciej.walecki@ppuc.pl>
2023-01-20 10:25:29 +0100

    style: add whitespace before if statement

--- src/app/features/create-event/validators/isHourInThePastValidator.ts
+++ src/app/features/create-event/validators/isHourInThePastValidator.ts
@@ -4,8 +4,10 @@ export const isHourInThePastValidator: ValidatorFn = (control: AbstractControl) 
   const today = new Date();
   const startDateRaw = control.get('startDate')?.value as string | undefined;
   const hour = control.get('hour')?.value as string | undefined;
+
   if (startDateRaw && hour) {
     const startDate = new Date(startDateRaw);
+
     if (startDate.toDateString() === today.toDateString()) {
       const hourSplitted = hour.split(':');
       return today.getHours() < +hourSplitted[0] ||
