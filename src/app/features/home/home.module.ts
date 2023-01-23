cc307913b9e0ac6df7cba8549cb3371038fd61e7
Grzegorz Bandyra <grzegorzbandyra@gmail.com>
2023-01-23 13:33:35 +0100

    feature-17-user-przeglada-grupy
    * feat: add initial group list display
    
    * fix: add margin to filtr div
    
    * feat: add loading and error display
    
    * feat: add group list display
    
    * fix: api url and palette import
    
    * fix: api url inject token
    
    * fix: api url injection to service
    
    * fix: add route to api url
    
    * fix: api url route
    
    Co-authored-by: Grzegorz Bandyra <grzegorz.bandyra@ppuc.pl>

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -10,6 +10,10 @@ import { HomeComponent } from './home.component';
         component: HomeComponent,
         children: [
           {
+            path: 'groups',
+            loadChildren: () => import('../group/group-list/group-list.module'),
+          },
+          {
             path: 'create-event',
             loadChildren: () => import('../create-event/create-event.module'),
           },
