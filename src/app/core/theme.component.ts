f357bc764fe5f2cd37f1a7ea987dc10206ac0a7c
Maciej Walecki <maciej.walecki@ppuc.pl>
2023-01-23 11:59:45 +0100

    fix: add cow loader to theme

--- src/app/core/theme.component.ts
+++ src/app/core/theme.component.ts
@@ -12,7 +12,7 @@ import { VisitorFormComponent } from '../features/visitor-form/visitor-form.comp
   template: `
     <h1>Storybook-like route</h1>
     <h2>Loader</h2>
-
+    <app-cow-loader></app-cow-loader>
     <h2>Pojedyncza grupa w liście</h2>
     <app-group-list-item
       [groupItem]="{ id: 0, name: 'Nowa grupa', description: 'Bardzo fajna nowa grupa' }"></app-group-list-item>
