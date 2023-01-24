bbdc0ebc81c3763ea240ca3ec0d96f150f4ce025
Maciej Walecki <79579229+maciekw129@users.noreply.github.com>
2023-01-24 12:40:03 +0100

    Feature 40 stworzenie searchbara (#111)
    * feat: add searchbar material component
    
    * feat: created service search method
    
    * refactor: changed search-bar directory
    
    * feat: add searchbar store
    
    * feat: add store effect funcionality
    
    * feat: connected user navbar with searchbar
    
    * fix: changed null response display
    
    * fix: add cow loader to theme
    
    * fix: clean code
    
    * style: clean code
    
    * build: refactor from ngrx store to service
    
    * fix: add slash to endpoint
    
    Co-authored-by: Maciej Walecki <maciej.walecki@ppuc.pl>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { environment } from 'src/environment';
 import { RouterModule } from '@angular/router';
 import { noProductionGuard } from '@shared/no-production.guard';
 import { AppInputValidatorDirective } from '@shared/inputValidator.directive';
-import { NavbarComponent } from './shared/user-navbar/navbar.component';
 import { MatIconModule } from '@angular/material/icon';
 import { Error404Component } from './shared/error404/error404.component';
 
@@ -31,7 +30,6 @@ import { Error404Component } from './shared/error404/error404.component';
     AppInputValidatorDirective,
     BrowserModule,
     MatIconModule,
-    NavbarComponent,
     RouterModule,
     HttpClientModule,
     StoreModule.forRoot({}),
