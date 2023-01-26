632c082787524eea2c6483c0ac14a34f1ffac87c
Jakub Borkowski <96788464+JRRBorkowski@users.noreply.github.com>
2023-01-26 14:30:01 +0100

    Feature #4 user moze wyswietlic wszystkie wydarzenia do ktorych ma dostep (#113)
    * feat: shell for list of events
    
    * feat: showing list of events
    
    * feat: showing list of events
    
    * fix: added module and proper observable
    
    * fix: fixed database removed needless code and changed event card styling
    
    * fix: added routing to create event for button
    
    * fix: package-lock fix
    
    * fix: removed needless code
    
    * fix: removed json server from package
    
    Co-authored-by: Jakub Borkowski <jakub.borkowski@ppuc.pl>

--- src/app/features/event/index.ts
+++ src/app/features/event/index.ts
@@ -1,2 +1,4 @@
 export * from './event.interfaces';
 export * from './event-card/event-card.component';
+export * from './event-list/event-list.component';
+export * from './event-list/event-list.module';
