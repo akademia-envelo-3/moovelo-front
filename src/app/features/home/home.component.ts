0aa43cb8c7811d2c266f93d2930ce81454bfdf72
Grzegorz Bandyra <grzegorzbandyra@gmail.com>
2023-01-26 15:12:29 +0100

    fix: merge conflict in home component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -3,12 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-<<<<<<< HEAD
-    application shell
-
-=======
     <app-navbar></app-navbar>
->>>>>>> 632c082787524eea2c6483c0ac14a34f1ffac87c
     <router-outlet></router-outlet>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
