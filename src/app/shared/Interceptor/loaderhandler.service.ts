3ac006044e118704e6935657a566e845e16231c2
Anna Kierlin <anna.kierlin@ppuc.pl>
2023-01-30 08:41:23 +0100

    feat: added generic type to get method

--- src/app/shared/Interceptor/loaderhandler.service.ts
+++ src/app/shared/Interceptor/loaderhandler.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { requestUrlWithoutLoader } from './requestHelperForLoader';
 @Injectable({
@@ -7,11 +8,9 @@ import { requestUrlWithoutLoader } from './requestHelperForLoader';
 })
 export class LoaderService {
   private http = inject(HttpClient);
-  noLoaderGETMethod(url: string) {
-    return this.http.get(requestUrlWithoutLoader(url));
-  }
   private isLoading$$ = new BehaviorSubject(false);
   private jobs: number[] = [];
+
   get isLoading$() {
     return this.isLoading$$.asObservable();
   }
@@ -25,5 +24,9 @@ export class LoaderService {
     if (this.jobs.length === 0) {
       this.isLoading$$.next(false);
     }
+  }
+
+  noLoaderGETMethod<Type>(url: string) {
+    return this.http.get<Type>(requestUrlWithoutLoader(url));
   }
 }
