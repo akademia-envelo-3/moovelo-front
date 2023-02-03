490c81f7cbd116cea22ab8979f6e3eab1f166149
Anna Kierlin <anna.kierlin@ppuc.pl>
2023-02-03 08:02:58 +0100

    feat: loader added to components

--- src/app/features/group/single-group/group-participants/group-participants.component.ts
+++ src/app/features/group/single-group/group-participants/group-participants.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ErrorComponent } from '@shared/error.component';
+import { ErrorhandlerService } from '@shared/Interceptor/errorhandler.service';
 import { CowLoaderComponent } from '@shared/loader/cow-loader.component';
 import { GroupUsersService } from './group-participants.service';
 
 @Component({
   standalone: true,
   selector: 'app-group-participants',
-  imports: [CommonModule, CowLoaderComponent],
+  imports: [CommonModule, CowLoaderComponent, ErrorComponent],
   templateUrl: './group-participants.html',
   styleUrls: ['./group-participants.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,5 +18,8 @@ export default class GroupParticipantsComponent {
   private route = inject(ActivatedRoute);
   private groupUsersService = inject(GroupUsersService);
   private groupId = this.route.snapshot.params['id'];
+  private errorService = inject(ErrorhandlerService);
+
+  errorClientServer$ = this.errorService.error$;
   users$ = this.groupUsersService.getParticipants(this.groupId);
 }
