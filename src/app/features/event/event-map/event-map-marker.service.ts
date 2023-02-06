96db2045771050a726527dc7f7a9b06fdcfc568d
LukaszPietrzykowski16 <lukaszpietrzykowsk@gmail.com>
2023-02-06 11:15:35 +0100

    fix: fixing typescript errors

--- src/app/features/event/event-map/event-map-marker.service.ts
+++ src/app/features/event/event-map/event-map-marker.service.ts
@@ -5,13 +5,16 @@ import * as leaflet from 'leaflet';
   providedIn: 'root',
 })
 export class EventMapMarkerService {
-  private markerCoordinates: any = undefined;
+  private markerCoordinates: { altitude: number; latitude: number } | undefined;
 
   passCoordinates(coordinates: any) {
     this.markerCoordinates = coordinates;
   }
 
   addCircle() {
+    if (this.markerCoordinates === undefined) {
+      return;
+    }
     const circle = leaflet.circle([this.markerCoordinates.altitude, this.markerCoordinates.latitude], {
       color: '#bd1c1c',
       fillColor: '#bd1c1c',
--- src/app/features/event/event-map/event-map.component.ts
+++ src/app/features/event/event-map/event-map.component.ts
@@ -15,7 +15,7 @@ export class EventMapComponent {
   private map: leaflet.Map | leaflet.LayerGroup<any> | undefined;
   private eventMapMarkerService = inject(EventMapMarkerService);
   isCoordinates = true;
-  @Input() coordinates: any;
+  @Input() coordinates!: { altitude: number | null; latitude: number | null };
 
   get getCoordinates() {
     return this.coordinates;
@@ -23,6 +23,10 @@ export class EventMapComponent {
 
   private initMap() {
     this.isCoordinates = true;
+
+    if (this.coordinates.altitude === null || this.coordinates.latitude === null) {
+      return;
+    }
     this.map = leaflet.map('map', {
       center: [this.coordinates.altitude, this.coordinates.latitude],
       zoom: 17,
@@ -34,7 +38,7 @@ export class EventMapComponent {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     });
 
-    this.eventMapMarkerService.addCircle().addTo(this.map);
+    this.eventMapMarkerService.addCircle()?.addTo(this.map);
 
     tiles.addTo(this.map);
   }
@@ -43,10 +47,9 @@ export class EventMapComponent {
     if (this.coordinates.altitude === null || this.coordinates.latitude === null) {
       this.isCoordinates = false;
       return;
-    } else {
-      this.eventMapMarkerService.passCoordinates(this.coordinates);
-      this.initMap();
     }
+    this.eventMapMarkerService.passCoordinates(this.coordinates);
+    this.initMap();
   }
 
   ngOnInit() {
