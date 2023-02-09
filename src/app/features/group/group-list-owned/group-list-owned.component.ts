3de5e41825033bacec4190683a1984fd42158d3e
Grzegorz Bandyra <czesiox8@gmail.com>
2023-02-09 13:31:32 +0100

    feat: unsubscribe on destroy

--- src/app/features/group/group-list-owned/group-list-owned.component.ts
+++ src/app/features/group/group-list-owned/group-list-owned.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ErrorComponent } from '@shared/error.component';
 import { ErrorhandlerService } from '@shared/Interceptor/errorhandler.service';
 import { CowLoaderComponent } from '@shared/loader/cow-loader.component';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { AppState } from 'src/app/app.module';
 import { GroupItemComponent } from '../group-list-item/group-list-item.component';
 import { GroupListItem } from '../group.interface';
@@ -18,18 +18,29 @@ import { GroupListOwnedService } from './group-list-owned.service';
   providers: [GroupListOwnedService],
   imports: [GroupItemComponent, CommonModule, ErrorComponent, CowLoaderComponent],
 })
-export default class GroupListOwnedComponent implements OnInit {
+export default class GroupListOwnedComponent implements OnInit, OnDestroy {
   private groupListOwnedService = inject(GroupListOwnedService);
   private errorService = inject(ErrorhandlerService);
   private store = inject<Store<AppState>>(Store);
+  private subscription = new Subscription();
 
   groupListOwned$: Observable<GroupListItem[]> | null = null;
   error$ = this.errorService.error$;
   userId$ = this.store.select(state => state.user);
 
-  ngOnInit() {
-    this.userId$.subscribe(result => {
+  getGroupListOwned() {
+    const sub = this.userId$.subscribe(result => {
       this.groupListOwned$ = this.groupListOwnedService.getOwnedGroups(result.id);
     });
+
+    this.subscription.add(sub);
+  }
+
+  ngOnInit() {
+    this.getGroupListOwned();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
