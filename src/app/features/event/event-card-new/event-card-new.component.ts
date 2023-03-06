4fecc4c555cfa12c7c3ccc8036b0e5e1d3faa514
Maciej Walecki <maciej.walecki@ppuc.pl>
2023-03-06 15:09:18 +0100

    fix: fixed responsive

--- src/app/features/event/event-card-new/event-card-new.component.ts
+++ src/app/features/event/event-card-new/event-card-new.component.ts
@@ -1,7 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  inject,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { EventCard } from '../event.interfaces';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 export interface GroupItem {
   id: number;
@@ -13,10 +22,26 @@ export interface GroupItem {
   selector: 'app-event-card-new[eventCard]',
   standalone: true,
   templateUrl: './event-card-new.component.html',
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, MatTooltipModule],
   styleUrls: ['./event-card-new.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EventCardNewComponent {
   @Input() eventCard!: EventCard;
+  @ViewChild('card') card!: ElementRef<HTMLDivElement>;
+  @ViewChild('hashtagsContainer') hashtagsContainer!: ElementRef<HTMLUListElement>;
+
+  private ref = inject(ChangeDetectorRef);
+
+  isFullWidth = true;
+
+  ngAfterViewInit() {
+    const card = this.card.nativeElement;
+    const hashtagsContainer = this.hashtagsContainer.nativeElement;
+
+    if (hashtagsContainer.offsetWidth > card.offsetWidth) {
+      this.isFullWidth = false;
+      this.ref.detectChanges();
+    }
+  }
 }
