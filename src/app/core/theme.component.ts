f86d13c7a597397eb58bcb7a01f5477cce48e520
Maciej Walecki <maciej.walecki@ppuc.pl>
2023-03-09 10:33:52 +0100

    fix: revert theme from develo

--- src/app/core/theme.component.ts
+++ src/app/core/theme.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
-import {
-  EventCard,
-  EventCardComponent,
-  EventParticipantsComponent,
-  EventParticipant,
-  EventParticipantsStatus,
-  EventSurveyComponent,
-  EventComment,
-} from '../features/event';
+import { EventCard, EventCardComponent, EventComment, EventSurvey } from '../features/event';
 import { GroupItemComponent } from '../features/group';
 import { CowLoaderComponent } from '../shared/loader/cow-loader.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { VisitorFormComponent } from '../features/visitor-form/visitor-form.component';
 import { GroupListItem } from '../features/group';
+import { EventSurveyComponent } from '../features/event/event-survey/event-survey.component';
 import CategoryListItemComponent from '../features/category/category-list-item/category-list-item.component';
 import { CategoryItemResponse } from '../features/category/category.interface';
 import { EventCommentsSingleComponent } from '../features/event/event-comment/event-comment-single/event-comments-single.component';
@@ -25,7 +18,6 @@ import { EventCommentsSingleComponent } from '../features/event/event-comment/ev
     CowLoaderComponent,
     FooterComponent,
     VisitorFormComponent,
-    EventParticipantsComponent,
     EventSurveyComponent,
     CategoryListItemComponent,
     EventCommentsSingleComponent,
@@ -37,69 +29,23 @@ import { EventCommentsSingleComponent } from '../features/event/event-comment/ev
     <app-cow-loader></app-cow-loader>
     <h2>Pojedyncza grupa w liście</h2>
     <app-group-list-item [groupItem]="group"></app-group-list-item>
-
     <h2>Event Card Component</h2>
     <div style="padding: 8px 8px;">
       <app-event-card [eventCard]="event"></app-event-card>
     </div>
-
     <h2>Formularz zapisu dla visitora</h2>
     <app-visitor-form></app-visitor-form>
-
-    <h2>Lista uczestników eventu</h2>
-    <app-event-participants [eventParticipants]="eventParticipants" [eventVisitors]="visitor"> </app-event-participants>
-
-    <h2>Ankiety</h2>
+    <h2>Widok ankiety dla usera</h2>
     <app-event-survey></app-event-survey>
-
     <h2>Category list Item</h2>
     <app-category-list-item [categoryItem]="categoryList"></app-category-list-item>
-
-    <h2>Komentarze</h2>
-    <app-event-comments-single [comment]="comment"></app-event-comments-single>
-
     <h2>Footer</h2>
     <app-footer></app-footer>
+    <h2>Komentarz</h2>
+    <app-event-comments-single [comment]="comment"></app-event-comments-single>
   `,
 })
 export default class ThemeComponent {
-  eventParticipants: Record<EventParticipantsStatus, EventParticipant[]> = {
-    accepted: [
-      {
-        userId: 1,
-        firstName: 'Janek',
-        lastName: 'Kowalski',
-      },
-      {
-        userId: 2,
-        firstName: 'Janina',
-        lastName: 'Kowalska',
-      },
-    ],
-    pending: [
-      {
-        userId: 1,
-        firstName: 'Zbigniew',
-        lastName: 'Stonoga',
-      },
-    ],
-    rejected: [
-      {
-        userId: 1,
-        firstName: 'Tupac',
-        lastName: 'Shakur',
-      },
-    ],
-  };
-
-  visitor: EventParticipant[] = [
-    {
-      userId: 1,
-      firstName: 'Adam',
-      lastName: 'Małysz',
-    },
-  ];
-
   group: GroupListItem = {
     groupOwner: {
       userId: 1,
@@ -141,6 +87,53 @@ export default class ThemeComponent {
       acceptedStatusUsers: 10,
     },
   };
+
+  surveys: EventSurvey[] = [
+    {
+      question: 'test',
+      isMultipleChoice: false,
+      answers: [
+        {
+          id: 1,
+          value: 'Hello',
+          voted: 20,
+        },
+        {
+          id: 2,
+          value: 'Yo',
+          voted: 20,
+        },
+        {
+          id: 3,
+          value: 'Help',
+          voted: 20,
+        },
+      ],
+      yourAnswersIds: [],
+    },
+    {
+      question: 'Mięso?',
+      isMultipleChoice: true,
+      answers: [
+        {
+          id: 1,
+          value: 'Tak',
+          voted: 20,
+        },
+        {
+          id: 2,
+          value: 'Nie',
+          voted: 20,
+        },
+        {
+          id: 3,
+          value: 'Bardzo nie',
+          voted: 20,
+        },
+      ],
+      yourAnswersIds: [],
+    },
+  ];
 
   categoryList: CategoryItemResponse = {
     id: 1,
